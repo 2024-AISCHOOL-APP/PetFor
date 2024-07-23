308b641f5ac25a0aea28debdb17c91511562a2c9
eun-young2 <grit_ey@naver.com>
2024-07-23 10:30:34 +0900

    업체등록(사업자,전문의) 기능구현-은영

--- frontend/src/components/BusinessRegistration.js
+++ frontend/src/components/BusinessRegistration.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import './BusinessRegistration.css';
+import { useContext } from 'react';
+import { UserInfo } from '../UserInfo';
+import axios from '../axios';
 
 const BusinessRegistration = () => {
+    //const { userId } = useContext(UserInfo);
+    const userId = '은영' // 로그인 완료되면 userId 받아오기
+    const [storeName, setStoreName] = useState('');
+    const [doctorNumber, setDoctorNumber] = useState('');
+    const [businessNumber, setBusinessNumber] = useState('');
+    const [registerType, setRegisterType] = useState('');
     const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const handleSubmit = async (e) => {
+       try{ e.preventDefault();
         // 등록 로직 처리
+        const response = await axios.post('/list/business',{
+            userId: userId,
+            storeName : storeName,
+            doctorNumber : doctorNumber,
+            businessNumber : businessNumber,
+            registerType : registerType
+        })
         console.log('등록 완료');
-    };
+        response.data.success
+        ?navigate('/register')
+        :navigate('/business-registration')
+    } catch(error) {
+        console.error(error);
+    }
+}
 
     const handleBack = () => {
         navigate('/register-method');
@@ -21,12 +43,14 @@ const BusinessRegistration = () => {
             
             <form onSubmit={handleSubmit}>
                 <div className="input-group">
-                    <label htmlFor="name">이름</label>
-                    <input type="text" id="name" placeholder="이름을 입력하세요" required />
+                    <label htmlFor="name">사업장 이름</label>
+                    <input type="text" id="name" placeholder="이름을 입력하세요" required 
+                    onChange={e=>setStoreName(e.target.value)}/>
                 </div>
                 <div className="input-group">
                     <label htmlFor="business-number">사업자번호</label>
-                    <input type="text" id="business-number" placeholder="사업자번호를 입력하세요" required />
+                    <input type="text" id="business-number" placeholder="사업자번호를 입력하세요" required 
+                    onChange={e=>setBusinessNumber(e.target.value)}/>
                 </div>
                 <button type="submit" className="submit-button">등록</button>
             </form>
