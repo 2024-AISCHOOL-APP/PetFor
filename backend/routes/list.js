3f57beb9b89fbc222a783eac49403b0d7c35d158
JaeHwanLim99 <oky208@naver.com>
2024-07-24 11:46:17 +0900

    임재환 커뮤니티 글 작성일 기준 정렬

--- backend/routes/list.js
+++ backend/routes/list.js
@@ -2,134 +2,133 @@ const express = require('express');
 const router = express.Router();
 const conn = require('../config/database');
 
-router.post('/store',(req,res)=>{
-    console.log('store data',req.body);
+router.post('/store', (req, res) => {
+    console.log('store data', req.body);
 
-    let sql = 'SELECT store_name FROM register where business_number IS NOT NULL';
-    conn.query(sql,(err, rows) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.json(rows);
-    });
-})
-
-router.post('/business', (req, res)=>{
-    console.log('business Data', req.body);
-    const {userId, storeName, doctorNumber, businessNumber, registerType} = req.body;
-
-    const sql = `SELECT * FROM register WHERE user_id = ?`
-    conn.query(sql, [userId], (err, rows)=>{
-        console.log('rows : ', rows);
-        if (rows.length>0) {
-            const sql = `SELECT register_type FROM register WHERE user_id = ?`
-            conn.query(sql, [userId], (err, rows)=>{
-                const register_type = rows[0].register_type;
-                if (register_type==='B'){
-                    const sql = `UPDATE register SET business_number = ?, store_name = ? WHERE user_id = ?`
-                    conn.query(sql, [businessNumber, storeName, userId], (err,rows)=>{
-                        if (rows) {
-                            res.json({ success: true })
-                        } else {
-                            console.error(err);
-                            res.json({ success: false })
-                        }
-                    })
-                } else {
-                    const sql = `UPDATE register SET register_type = 'C' , business_number = ? WHERE user_id = ?`
-                    conn.query(sql,[businessNumber,userId],(err,rows)=>{
-                        if (rows) {
-                            res.json({ success: true })
-                        } else {
-                            console.error(err);
-                            res.json({ success: false })
-                        }
-                    })
-                }
-            })
-        } else {
-            const sql = `INSERT INTO register(user_id,store_name,business_number,register_type,register_date) VALUES (?, ?, ?, 'B' ,current_timestamp())`;
-
-            conn.query(sql, [userId, storeName, businessNumber], (err, rows) => {
-                if (rows) {
-                    res.json({ success: true })
-                } else {
-                    console.error(err);
-                    res.json({ success: false })
-                }
-            })
-        }
-    })
-    
-})
-
-router.post('/professional', (req, res)=>{
-    console.log('professional Data', req.body);
-    const {userId, storeName, doctorNumber, businessNumber, registerType} = req.body;
-
-    const sql = `SELECT * FROM register WHERE user_id = ?`
-    conn.query(sql, [userId], (err, rows)=>{
-        if (rows.length>0) {
-            const sql = `SELECT register_type FROM register WHERE user_id = ?`
-            conn.query(sql, [userId], (err, rows)=>{
-                const register_type = rows[0].register_type;
-                if (register_type==='D'){
-                    const sql = `UPDATE register SET doctor_number = ?, store_name = ? WHERE user_id = ?`
-                    conn.query(sql, [doctorNumber, storeName, userId], (err,rows)=>{
-                        if (rows) {
-                            res.json({ success: true })
-                        } else {
-                            console.error(err);
-                            res.json({ success: false })
-                        }
-                    })
-                } else {
-                    const sql = `UPDATE register SET register_type = 'C' , doctor_number = ? WHERE user_id = ?`
-                    conn.query(sql,[doctorNumber,userId],(err,rows)=>{
-                        if (rows) {
-                            res.json({ success: true })
-                        } else {
-                            console.error(err);
-                            res.json({ success: false })
-                        }
-                    })
-                }
-            })
-        } else {
-            const sql = `INSERT INTO register(user_id,store_name,doctor_number,register_type,register_date) VALUES (?, ?, ?, 'D' ,current_timestamp())`;
-
-            conn.query(sql, [userId, storeName, doctorNumber], (err, rows) => {
-                if (rows) {
-                    res.json({ success: true })
-                } else {
-                    console.error(err);
-                    res.json({ success: false })
-                }
-            })
-        }
-    })
-})
-
-router.post('/post',(req,res)=>{
-    let sql = 'SELECT * FROM community';
+    let sql = 'SELECT store_name FROM register WHERE business_number IS NOT NULL';
     conn.query(sql, (err, rows) => {
         if (err) {
             return res.status(500).send(err);
         }
         res.json(rows);
     });
-})
+});
 
-router.post('/writepost', (req, res)=>{
+router.post('/business', (req, res) => {
+    console.log('business Data', req.body);
+    const { userId, storeName, doctorNumber, businessNumber, registerType } = req.body;
+
+    const sql = `SELECT * FROM register WHERE user_id = ?`;
+    conn.query(sql, [userId], (err, rows) => {
+        console.log('rows : ', rows);
+        if (rows.length > 0) {
+            const sql = `SELECT register_type FROM register WHERE user_id = ?`;
+            conn.query(sql, [userId], (err, rows) => {
+                const register_type = rows[0].register_type;
+                if (register_type === 'B') {
+                    const sql = `UPDATE register SET business_number = ?, store_name = ? WHERE user_id = ?`;
+                    conn.query(sql, [businessNumber, storeName, userId], (err, rows) => {
+                        if (rows) {
+                            res.json({ success: true });
+                        } else {
+                            console.error(err);
+                            res.json({ success: false });
+                        }
+                    });
+                } else {
+                    const sql = `UPDATE register SET register_type = 'C', business_number = ? WHERE user_id = ?`;
+                    conn.query(sql, [businessNumber, userId], (err, rows) => {
+                        if (rows) {
+                            res.json({ success: true });
+                        } else {
+                            console.error(err);
+                            res.json({ success: false });
+                        }
+                    });
+                }
+            });
+        } else {
+            const sql = `INSERT INTO register(user_id, store_name, business_number, register_type, register_date) VALUES (?, ?, ?, 'B', current_timestamp())`;
+
+            conn.query(sql, [userId, storeName, businessNumber], (err, rows) => {
+                if (rows) {
+                    res.json({ success: true });
+                } else {
+                    console.error(err);
+                    res.json({ success: false });
+                }
+            });
+        }
+    });
+});
+
+router.post('/professional', (req, res) => {
+    console.log('professional Data', req.body);
+    const { userId, storeName, doctorNumber, businessNumber, registerType } = req.body;
+
+    const sql = `SELECT * FROM register WHERE user_id = ?`;
+    conn.query(sql, [userId], (err, rows) => {
+        if (rows.length > 0) {
+            const sql = `SELECT register_type FROM register WHERE user_id = ?`;
+            conn.query(sql, [userId], (err, rows) => {
+                const register_type = rows[0].register_type;
+                if (register_type === 'D') {
+                    const sql = `UPDATE register SET doctor_number = ?, store_name = ? WHERE user_id = ?`;
+                    conn.query(sql, [doctorNumber, storeName, userId], (err, rows) => {
+                        if (rows) {
+                            res.json({ success: true });
+                        } else {
+                            console.error(err);
+                            res.json({ success: false });
+                        }
+                    });
+                } else {
+                    const sql = `UPDATE register SET register_type = 'C', doctor_number = ? WHERE user_id = ?`;
+                    conn.query(sql, [doctorNumber, userId], (err, rows) => {
+                        if (rows) {
+                            res.json({ success: true });
+                        } else {
+                            console.error(err);
+                            res.json({ success: false });
+                        }
+                    });
+                }
+            });
+        } else {
+            const sql = `INSERT INTO register(user_id, store_name, doctor_number, register_type, register_date) VALUES (?, ?, ?, 'D', current_timestamp())`;
+
+            conn.query(sql, [userId, storeName, doctorNumber], (err, rows) => {
+                if (rows) {
+                    res.json({ success: true });
+                } else {
+                    console.error(err);
+                    res.json({ success: false });
+                }
+            });
+        }
+    });
+});
+
+router.post('/post', (req, res) => {
+    let sql = 'SELECT * FROM community ORDER BY posting_date DESC';
+    conn.query(sql, (err, rows) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.json(rows);
+    });
+});
+
+router.post('/writepost', (req, res) => {
     let sql = `INSERT INTO community (user_id, title, content, posting_date) VALUES (?, ?, ?, current_timestamp())`;
     conn.query(sql, [req.body.userId, req.body.title, req.body.content], (err, rows) => {
         if (err) {
-            res.json({success : false})
+            res.json({ success: false });
             return res.status(500).send(err);
         }
-        res.json({success : true});
-    })
-})
+        res.json({ success: true });
+    });
+});
 
 // 포스트 세부 정보 가져오기
 router.get('/post/:id', (req, res) => {
